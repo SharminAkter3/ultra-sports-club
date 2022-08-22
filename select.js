6f57e36bbdcc78f7fd060d3d2464f2e4804fdf62
SharminAkter3 <mssharmin4145@gmail.com>
2022-08-22 09:21:14 -0700

    added-budget-section

---
+++ select.js
@@ -0,0 +1,37 @@
+const playerArry = [];
+
+
+function display(player) {
+
+    const tableBody = document.getElementById('player-table');
+    tableBody.innerHTML = "";
+
+    for (let i = 0; i < player.length; i++) {
+        const name = playerArry[i].playerName;
+
+        const tr = document.createElement("tr");
+        tr.innerHTML =
+            `
+            <th>${1 + i} </th>
+            <th>${name}  </td>
+        `;
+        tableBody.appendChild(tr);
+    }
+
+
+}
+
+
+function selectedV(element) {
+
+    const playerName = element.parentNode.children[0].innerText;
+    const playerObj = {
+        playerName: playerName
+    }
+    playerArry.push(playerObj);
+
+    display(playerArry);
+
+
+    element.disabled = true;
+}
